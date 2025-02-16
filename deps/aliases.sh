6221b3f91573342feecd713c7980671a4032add1
davidheineman <dheineman3@gatech.edu>
2025-02-15 23:22:35 -0800

    add log streaming

--- deps/aliases.sh
+++ deps/aliases.sh
@@ -12,6 +12,8 @@ bport() {
 alias bd='python /Users/dhei/ai2/beaker_image/deps/get_jobs.py --username davidh --sessions-only' # describe sessions
 alias bdall='python /Users/dhei/ai2/beaker_image/deps/get_jobs.py --username davidh' # describe all jobs
 alias bl='python /Users/dhei/ai2/beaker_image/deps/launcher.py' # launch session
+alias blogs='python /Users/dhei/ai2/beaker_image/deps/stream_logs.py -j' # launch session
+alias bstream='python /Users/dhei/ai2/beaker_image/deps/stream_logs.py -s -j' # launch session
 
 bpriority() {
     if [[ $# -lt 2 ]]; then
---
+++ deps/stream_logs.py
@@ -0,0 +1,61 @@
+from datetime import timedelta
+import argparse
+import sys
+import warnings
+
+# Suppress cryptography deprecation warnings
+from cryptography.utils import CryptographyDeprecationWarning
+warnings.filterwarnings('ignore', category=CryptographyDeprecationWarning)
+
+from beaker import Beaker
+
+def stream_experiment_logs(job_id: str, do_stream: bool):
+    # Initialize the Beaker client
+    beaker = Beaker.from_env()
+    
+    # Get the job first
+    job = beaker.job.get(job_id)
+
+    # Get the experiment ID
+    if job.execution is not None:
+        experiment_id = job.execution.experiment
+    else:
+        if job.kind == 'session':
+            raise ValueError('Job is a session. Please provide an execution job.')
+        raise RuntimeError(job)
+    
+    try:
+        if do_stream:
+            for line in beaker.experiment.follow(
+                experiment_id,
+                strict=True,
+                # since=timedelta(minutes=2)
+            ):
+                print(line.decode().rstrip(), flush=True)
+        else:
+            log_stream = beaker.experiment.logs(
+                experiment_id, 
+                quiet=True,
+                # since=timedelta(minutes=2)
+            )
+            
+            for line in log_stream:
+                log_line = line.decode().rstrip()
+                print(log_line)
+                
+                sys.stdout.flush()
+            
+    except KeyboardInterrupt:
+        print("\nLog streaming interrupted by user")
+    except Exception as e:
+        print(f"Error streaming logs: {e}")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Stream logs from a Beaker job")
+    parser.add_argument("-j", "--job_id", help="The ID or name of the Beaker job", required=True)    
+    parser.add_argument("-s", "--stream", help="The ID or name of the Beaker job", action="store_true", default=False)    
+    
+    args = parser.parse_args()
+    
+    stream_experiment_logs(args.job_id, do_stream=args.stream)
