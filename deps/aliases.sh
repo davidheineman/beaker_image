d7bee9d9e630fd851de1fd179f7b58074c4461f1
davidheineman <dheineman3@gatech.edu>
2025-02-16 10:48:46 -0800

    fix alias paths

--- deps/aliases.sh
+++ deps/aliases.sh
@@ -1,6 +1,6 @@
 # Relevant beaker scripts
 
-BEAKER_IMG_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+BEAKER_DEPS_DIR="$(cd "$(dirname "${BASH_SOURCE[0]:-${(%):-%x}}")" && pwd)"
 
 alias ai2="ssh ai2"
 alias bstop='beaker session stop'
@@ -8,14 +8,14 @@ blist() {
     beaker session list --all --author davidh | grep running
 }
 bport() {
-    source $BEAKER_IMG_DIR/deps/update_port.sh
+    source $BEAKER_DEPS_DIR/update_port.sh
 }
 # alias bd='beaker session describe'
-alias bd='python '$BEAKER_IMG_DIR'/deps/get_jobs.py --username davidh --sessions-only' # describe sessions
-alias bdall='python '$BEAKER_IMG_DIR'/deps/get_jobs.py --username davidh' # describe all jobs
-alias bl='python '$BEAKER_IMG_DIR'/deps/launcher.py' # launch session
-alias blogs='python '$BEAKER_IMG_DIR'/deps/stream_logs.py -j' # launch session
-alias bstream='python '$BEAKER_IMG_DIR'/deps/stream_logs.py -s -j' # launch session
+alias bd='python '$BEAKER_DEPS_DIR'/get_jobs.py --username davidh --sessions-only' # describe sessions
+alias bdall='python '$BEAKER_DEPS_DIR'/get_jobs.py --username davidh' # describe all jobs
+alias bl='python '$BEAKER_DEPS_DIR'/launcher.py' # launch session
+alias blogs='python '$BEAKER_DEPS_DIR'/stream_logs.py -j' # launch session
+alias bstream='python '$BEAKER_DEPS_DIR'/stream_logs.py -s -j' # launch session
 
 bpriority() {
     if [[ $# -lt 2 ]]; then
