64739ed8721ee76d511fa8137f9d5c5b20787efe
davidheineman <dheineman3@gatech.edu>
2025-05-16 15:19:08 -0700

    add pre-auths

--- src/.bashrc
+++ src/.bashrc
@@ -116,6 +116,22 @@ fi
 alias vscodereset="rm -rf ~/.vscode-server/cli/servers"
 
 if [ ! -f /root/.ssh/environment ]; then
+    # SLOW: Copy env variables from docker process (such as HF_TOKEN)
+    # process_info=$(ps -e -o user,pid,cmd | grep "/usr/sbin/sshd -D" | grep "^root")
+    # pids=$(echo "$process_info" | awk '{print $2}')
+    # for pid in $pids; do
+    #     env_vars=$(cat /proc/$pid/environ 2>/dev/null | tr '\0' '\n')
+    #     for env_var in $env_vars; do
+    #         key=$(echo "$env_var" | cut -d= -f1)
+    #         value=$(echo "$env_var" | cut -d= -f2-)
+    #         if [[ "$key" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
+    #             if [ -z "${!key}" ]; then
+    #                 export "$env_var"
+    #             fi
+    #         fi
+    #     done
+    # done
+
     # Fast version of env copying (assuming OpenSSH is process 1)
     if [ -r /proc/1/environ ]; then
         env_vars=$(cat /proc/1/environ 2>/dev/null | tr "\0" "\n")
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,37 +1,16 @@
 #!/bin/bash
 
-# SLOW: Copy env variables from docker process (such as HF_TOKEN)
-# process_info=$(ps -e -o user,pid,cmd | grep "/usr/sbin/sshd -D" | grep "^root")
-# pids=$(echo "$process_info" | awk '{print $2}')
-# for pid in $pids; do
-#     env_vars=$(cat /proc/$pid/environ 2>/dev/null | tr '\0' '\n')
-#     for env_var in $env_vars; do
-#         key=$(echo "$env_var" | cut -d= -f1)
-#         value=$(echo "$env_var" | cut -d= -f2-)
-#         if [[ "$key" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
-#             if [ -z "${!key}" ]; then
-#                 export "$env_var"
-#             fi
-#         fi
-#     done
-# done
+source /root/ai2/.bashrc
 
-# Copy env vars from Beaker process
-if [ ! -f /root/.ssh/environment ]; then
-    if [ -r /proc/1/environ ]; then
-        env_vars=$(cat /proc/1/environ 2>/dev/null | tr "\0" "\n")
-        for env_var in $env_vars; do
-            key=$(echo "$env_var" | cut -d= -f1)
-            value=$(echo "$env_var" | cut -d= -f2-)
-            if [[ "$key" =~ ^[a-zA-Z_][a-zA-Z0-9_]*$ ]]; then
-                if [ -z "${!key}" ]; then
-                    export "$env_var"
-                fi
-            fi
-        done
-    fi
-    printenv > /root/.ssh/environment
-fi
+# Authenticate with git
+gitlogin
+gcplogin
+
+# Start VS code servers
+export PATH="/root/.vscode-server/bin:/root/.vscode-server/bin/bin:$PATH"
+export PATH="/root/.cursor-server/bin:/root/.cursor-server/bin/bin:$PATH"
+nohup code-server --bind-addr 0.0.0.0:8080 --accept-server-license-terms > /tmp/code-server.log 2>&1 &
+# nohup cursor-server --bind-addr 0.0.0.0:8080 --accept-server-license-terms > /tmp/cursor-server.log 2>&1 &
 
 # Start OpenSSH server
 exec /usr/sbin/sshd -D
