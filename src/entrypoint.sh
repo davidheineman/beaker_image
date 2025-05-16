6aad938b8e55f33ea6985f7668fc190844676da8
davidheineman <dheineman3@gatech.edu>
2025-05-16 15:55:30 -0700

    remote code server still not working

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -7,9 +7,10 @@ gitlogin
 gcplogin
 
 # Start VS code servers
-export PATH="/root/.vscode-server/bin:/root/.vscode-server/bin/bin:$PATH"
-export PATH="/root/.cursor-server/bin:/root/.cursor-server/bin/bin:$PATH"
-nohup code-server --bind-addr 0.0.0.0:8080 --accept-server-license-terms > /tmp/code-server.log 2>&1 &
+# export PATH="/root/.vscode-server/bin:/root/.vscode-server/bin/bin:$PATH"
+# export PATH="/root/.cursor-server/bin:/root/.cursor-server/bin/bin:$PATH"
+# code-server --accept-server-license-terms --connection-token=remotessh --start-server
+# nohup code-server --bind-addr 0.0.0.0:8080 --accept-server-license-terms > /tmp/code-server.log 2>&1 &
 # nohup cursor-server --bind-addr 0.0.0.0:8080 --accept-server-license-terms > /tmp/cursor-server.log 2>&1 &
 
 # Start OpenSSH server
